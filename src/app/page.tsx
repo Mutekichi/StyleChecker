6fb8cb5b1af05ca2598a601ab4557bad6649bba0
tanomitsu2002 <64388900+tanomitsu@users.noreply.github.com>
2024-09-08 12:29:32 +0900

    feat: 生成結果を右に表示するように

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 "use client"
 
+import { AppearanceCheckResultView } from "@/components/AppearanceCheck"
 import { LeftPanel } from "@/components/LeftPanel"
-import { RightPanel, RightPanelProps } from "@/components/RightPanel"
+import { parseAppearanceCheck } from "@/features/Parse"
+import { AppearanceCheckProps } from "@/features/Parse/types"
 import { Situation, situationToPrompt } from "@/features/Situation"
 import { useClaude } from "@/hooks/useClaude"
 import {
@@ -13,19 +15,8 @@ import {
   useToast,
   VStack,
 } from "@chakra-ui/react"
-import { useCallback, useState } from "react"
+import { useCallback, useMemo, useState } from "react"
 import { FaCameraRetro } from "react-icons/fa"
-
-const rightPanelProps: RightPanelProps = {
-  advisorBubblePropsList: [
-    { emotion: "happy", text: "Hello!", isSpeaking: true },
-    { emotion: "angry", text: "Go away!" },
-    {
-      emotion: "neutral",
-      text: "吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。この書生というのは時々我々を捕えて煮て食うという話である。しかしその当時は何という考もなかったから別段恐しいとも思わなかった。ただ彼の掌に載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。掌の上で少し落ちついて書生の顔を見たのがいわゆる人間というものの見始であろう。この時妙なものだと思った感じが今でも残っている。第一毛をもって装飾されべきはずの顔がつるつるしてまるで薬缶だ。",
-    },
-  ],
-}
 
 export default function Home() {
   const {
@@ -74,6 +65,14 @@ export default function Home() {
     streamResponse(prompt)
   }, [image, situation, streamResponse, toast])
 
+  // Loading中はundefined, 生成後はパース結果となる
+  const checkResult = useMemo((): AppearanceCheckProps | undefined => {
+    if (isLoading) {
+      return undefined
+    }
+    return parseAppearanceCheck(output)
+  }, [isLoading, output])
+
   return (
     <VStack>
       <Text fontSize="xxx-large" fontWeight="bold">
@@ -101,7 +100,10 @@ export default function Home() {
           />
         </Box>
         <Box w="40%">
-          <RightPanel {...rightPanelProps} />
+          <AppearanceCheckResultView
+            isLoading={isLoading}
+            result={checkResult}
+          />
         </Box>
       </HStack>
       <IconButton
